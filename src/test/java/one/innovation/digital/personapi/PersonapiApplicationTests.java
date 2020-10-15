50cb8e4bb0ea4aa058627c1e2df0f3e75535aa5f
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-10-15 02:10:45 -0300

    Project Created + First Commit

---
+++ src/test/java/one/innovation/digital/personapi/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package one.innovation.digital.personapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
